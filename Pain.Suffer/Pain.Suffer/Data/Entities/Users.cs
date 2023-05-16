ddf08c4ad444b708e7531530b0715084f69ec499
Exapl3 <65217935+Exapl3@users.noreply.github.com>
2023-05-16 19:56:32 +0300

    Add files via upload

---
+++ Pain.Suffer/Pain.Suffer/Data/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Pain.Suffer.Data.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int? CountryCodeId { get; set; }
+        public string? PhoneNum { get; set; }
+        public string? Email { get; set; }
+        public int? FavoriteId { get; set; }
+        public int? CartId { get; set; }
+        public string? UserImageURL { get; set; }   
+        public int? Money { get; set; }
+    }
+}
